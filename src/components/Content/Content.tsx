f4d4d1cecd483fa8ee39563e26d0c09981c53dc5
AntonChislov <antonchislov6642@gmail.com>
2022-11-30 22:50:25 +0300

    react-router-dom

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import styles from './Content.module.css'
 import {ProfilePage} from "./ProfilePage/ProfilePage";
 import {DialogsPage} from "./DialogsPage/DialogsPage";
+import {Routes, Route} from "react-router-dom";
 
 export const Content = () => {
     return (
         <div className={styles.content}>
-            <ProfilePage/>
-            {/*<DialogsPage/>*/}
+            <Routes>
+                <Route path='/' element={<ProfilePage/>} />
+                <Route path='dialogs' element={<DialogsPage/>} />
+            </Routes>
         </div>
     )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import styles from './Navbar.module.css';
-
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
     return (
         <div className={styles.navbar}>
             <div className={styles.navbar_block}>
-                <div><a>Профиль</a></div>
-                <div><a>Сообщения</a></div>
-                <div><a>Друзья</a></div>
-                <div><a>Новости</a></div>
-                <div><a>Музыка</a></div>
-                <div><a>Настройки</a></div>
+                <div><NavLink to='/'>Профиль</NavLink></div>
+                <div><NavLink to='dialogs'>Сообщения</NavLink></div>
+                <div><NavLink to='friends'>Друзья</NavLink></div>
+                <div><NavLink to='news'>Новости</NavLink></div>
+                <div><NavLink to='music'>Музыка</NavLink></div>
+                <div><NavLink to='settings'>Настройки</NavLink></div>
             </div>
         </div>
     )
