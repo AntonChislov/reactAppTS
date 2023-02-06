aeca9adfb986e4c71b4a2155251c9dfe1479a257
AntonChislov <antonchislov6642@gmail.com>
2023-02-06 11:49:04 +0300

    commit

---
+++ src/components/Content/DialogsPage/DialogBlock/SendMessageBlock/SendMessageBlock.tsx
@@ -0,0 +1,25 @@
+import React, {FC} from "react";
+import styles from "../DialogsBlock.module.css";
+
+type AddMessageProps = {
+    sendMessage: (text: string) => void
+}
+
+export const SendMessageBlock: FC<AddMessageProps> = ({sendMessage}) => {
+
+    const ref = React.createRef<HTMLInputElement>()
+
+    const addMessage = () => {
+        debugger
+        if (ref.current) {
+            sendMessage(ref.current.value)
+        }
+    }
+
+    return (
+        <div className={styles.sendMessagesBlock}>
+            <input ref={ref} placeholder={'введи сообщение'}/>
+            <button onClick={addMessage}>отправить</button>
+        </div>
+    )
+}
